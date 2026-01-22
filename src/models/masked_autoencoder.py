322ffa06b2d03a62512cde91634fd11c2d61e099
Marta <268368@student.pwr.edu.pl>
2026-01-22 16:02:20 +0100

    small changes in training and testing

--- src/models/masked_autoencoder.py
+++ src/models/masked_autoencoder.py
@@ -29,7 +29,31 @@ class MAE(nn.Module):
         x_masked = x * mask
 
         return x_masked, mask
-
+    #
+    # def random_masking(self, x):  # patche
+    #     """
+    #     Maskowanie losowe na poziomie patchy.
+    #     Args:
+    #         x: Tensor obrazu  (B, C, H, W)
+    #     Returns:
+    #         x_masked: zamaskowany obraz (B, C, H, W)
+    #         mask: Tensor maski (B, 1, H, W)  1=widoczny, 0=zamaskowany
+    #     """
+    #     B, C, H, W = x.shape
+    #     patch_size = 4  # (32x32 -> 8x8 grid)
+    #
+    #     # wymiar siatki patchy
+    #     h_patches = H // patch_size
+    #     w_patches = W // patch_size
+    #
+    #     mask_patches = torch.rand(B, h_patches, w_patches, device=x.device) > self.masking_ratio
+    #     mask_patches = mask_patches.float()
+    #
+    #     mask = F.interpolate(mask_patches.unsqueeze(1), size=(H, W), mode='nearest')
+    #
+    #     x_masked = x * mask
+    #
+    #     return x_masked, mask
 
     def forward(self, x):
         x_masked, mask = self.random_masking(x)
